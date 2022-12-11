cdd3344e161ac76d226afd750c6ffb048190a295
Alain Levasseur <118285760+AlainLevasseur@users.noreply.github.com>
2022-12-11 13:39:36 -0500

    solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    if(start < 0 || end < 0 || typeof start !== 'number' || typeof end !== 'number') return "ERROR";
+
+    let smallest = start < end ? start : end;
+    let biggest = start > end ? start : end;
+    let sum = 0;
+
+    for(let i = smallest; i <= biggest; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
