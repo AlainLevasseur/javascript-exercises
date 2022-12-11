51c213d072feac402774de7c195347b528d2eb1e
Alain Levasseur <118285760+AlainLevasseur@users.noreply.github.com>
2022-12-11 13:14:48 -0500

    solve removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...options) {
+    return array.filter((element) => {
+        for(const removeItem of options) {
+            if(removeItem === element) return false;
+        }
+        return true;
+    })
 };
 
 // Do not edit below this line
