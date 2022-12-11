8489b490059938148ea4480f87e5a3adfcd27a86
Alain Levasseur <118285760+AlainLevasseur@users.noreply.github.com>
2022-12-11 12:59:39 -0500

    solve reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringArray = string.split("");
+    let reversedString ='';
+    for(let i = stringArray.length - 1; i >= 0; i--){
+        reversedString += stringArray[i];
+    }
+    return reversedString;
 
+    //return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
