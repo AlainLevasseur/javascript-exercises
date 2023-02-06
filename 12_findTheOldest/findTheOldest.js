485df8996e861124199e12ce465529764db2c8a5
Alain Levasseur <118285760+AlainLevasseur@users.noreply.github.com>
2023-02-06 15:40:45 -0500

    solve the findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        if (oldest === 0) return person;
+        if (getAge(oldest) < getAge(person)) return person;
+        return oldest;
+    }, 0)
 };
+
+const getAge = function(person) {
+    if(person.yearOfDeath === undefined) { 
+        return new Date().getFullYear() - person.yearOfBirth; 
+    }
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
