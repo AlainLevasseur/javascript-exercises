490e8f5165ddc68e8138547d68edfd992ab0e1d1
Alain Levasseur <118285760+AlainLevasseur@users.noreply.github.com>
2023-02-06 14:42:39 -0500

    solve palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,15 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let input = string.toLowerCase().replace(/\W/g, '');
+    console.log(input);
+    let head = 0;
+    let tail = input.length - 1;
+    do {
+        if(input.charAt(head) !== input.charAt(tail)) return false;
+        head++;
+        tail--;
+    } while (tail - head > 1)
 
+    return true;
 };
 
 // Do not edit below this line
